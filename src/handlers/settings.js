8bc26c35866153821a17fc1c4574327f19cfb594
Vyacheslav <slava021323@gmail.com>
2021-08-06 13:26:00 +0600

    1.0.4
    - Fixed settings error.

--- src/handlers/settings.js
+++ src/handlers/settings.js
@@ -8,34 +8,16 @@ module.exports = () => async (ctx) => {
         const user = await getUserSession(ctx);
         ctx.i18n.locale(user.language);
 
-        if (ctx.match) {
-            ctx.editMessageText(ctx.i18n.t('service.settings'), {
-                parse_mode: 'HTML',
-                reply_markup: Markup.inlineKeyboard([
-                    [Markup.callbackButton(ctx.i18n.t('button.language'), `language`)],
-                    [Markup.callbackButton(ctx.i18n.t('button.to_sticker', { state: user.to_sticker ? '✅' : '' }), `to_sticker`)],
-                    [
-                        Markup.urlButton(ctx.i18n.t('button.channel'), 'https://t.me/softik'),
-                        Markup.urlButton(ctx.i18n.t('button.support'), 'https://t.me/vychs')
-                    ]
-                ])
-            });
-
-            ctx.answerCbQuery();
-        } else {
-            ctx.replyWithHTML(ctx.i18n.t('service.settings'), {
-                reply_markup: Markup.inlineKeyboard([
-                    [Markup.callbackButton(ctx.i18n.t('button.language'), `language`)],
-                    [Markup.callbackButton(ctx.i18n.t('button.to_sticker', { state: user.to_sticker ? '✅' : '' }), `to_sticker`)],
-                    [
-                        Markup.urlButton(ctx.i18n.t('button.channel'), 'https://t.me/softik'),
-                        Markup.urlButton(ctx.i18n.t('button.support'), 'https://t.me/vychs')
-                    ]
-                ])
-            });
-        }
-
-
+        ctx.replyWithHTML(ctx.i18n.t('service.settings'), {
+            reply_markup: Markup.inlineKeyboard([
+                [Markup.callbackButton(ctx.i18n.t('button.language'), `language`)],
+                [Markup.callbackButton(ctx.i18n.t('button.to_sticker', { state: user.to_sticker ? '✅' : '' }), `to_sticker`)],
+                [
+                    Markup.urlButton(ctx.i18n.t('button.channel'), 'https://t.me/softik'),
+                    Markup.urlButton(ctx.i18n.t('button.support'), 'https://t.me/vychs')
+                ]
+            ])
+        });
     } catch (err) {
         console.error(err);
     }
