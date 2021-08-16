ae8d550a72cf70f4022c76c12ece45066f9ac9ca
Vyacheslav <slava021323@gmail.com>
2021-08-16 20:53:51 +0600

    1.2.13
    - Minor bug fixes.

--- src/scripts/checkSubscription.js
+++ src/scripts/checkSubscription.js
@@ -7,8 +7,6 @@ module.exports = (ctx) => {
     try {
         return ctx.telegram.getChatMember(config.channel, ctx.from.id).then(response => {
             const is_member = (response.status === 'member' || response.status === 'owner') ? true : false;
-            
-            User.updateOne({ id: ctx.from.id }, { $set: { channel_member: is_member } }, () => {});
 
             return is_member;
         }).catch(() => {});
