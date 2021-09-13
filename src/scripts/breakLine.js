b510014862b5b1ae7f16dea40cd85655389943b5
Vyacheslav <slava021323@gmail.com>
2021-09-13 21:23:52 +0600

    1.5.2
    - Minor bug fixes.

--- src/scripts/breakLine.js
+++ src/scripts/breakLine.js
@@ -2,7 +2,7 @@
 
 module.exports = (text) => {
     try {
-        const re = new RegExp('([\\w\\s]{' + (20 - 2) + ',}?\\w)\\s?\\b', 'g');
+        const re = new RegExp('([\\w\\s]{' + (18 - 2) + ',}?\\w)\\s?\\b', 'g');
         return text.replace(re, '$1\n');
     } catch (err) {
         console.error(err);
