563dc874c60b6afac2bc176a1d16832c8122e452
Vyacheslav <slava021323@gmail.com>
2021-10-30 03:26:32 +0600

    1.4.34
    - Bug fix.

--- src/bot.js
+++ src/bot.js
@@ -8,7 +8,7 @@ const session = require('telegraf/session');
 const path = require('path');
 const TelegrafI18n = require('telegraf-i18n');
 const i18n = new TelegrafI18n({
-    directory: path.resolve(__dirname, './src/locales'),
+    directory: path.resolve(__dirname, './locales'),
     defaultLanguage: 'en',
     defaultLanguageOnMissing: true
 });
